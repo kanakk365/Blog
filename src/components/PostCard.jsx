22d6f02c00ddb45fc46044da1046d29ad41ae883
kanakk365 <kanakkumarmahala000@gmail.com>
2024-10-03 02:19:03 +0530

    dont know

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,7 +5,7 @@ import appwriteServices from "../appwrite/config";
 function PostCard({ $id, featuredImage, title, content }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100  py-4 flex flex-row justify-between border-b-2 border-gray-400 sm:h-72">
+      <div className="w-full bg-gray-100  py-4 flex flex-row justify-between border-b-2 border-gray-400 sm:h-60">
         <div className="flex flex-col justify-between gap-2 w-3/4">
           <div>
             <h2 className=" text-xl sm:text-4xl font-bold">{title}</h2>
@@ -15,12 +15,10 @@ function PostCard({ $id, featuredImage, title, content }) {
             ></p>
           </div>
 
-          <button class=" w-32 cursor-pointer transition-all bg-blue-500 text-white px-4 rounded-lg py-2 rounded-lgborder-blue-600 first-line:border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
-            Read More...
-          </button>
+         
         </div>
 
-        <div className="justify-center mb-4 w-1/4 ">
+        <div className="justify-center mb-4 w-1/4">
           <img
             src={appwriteServices.getFilePreview(featuredImage)}
             alt={title}
