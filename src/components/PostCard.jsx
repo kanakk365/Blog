fbc02ec783ae2a03e0cb6799bfd198686482ed5b
kanakk365 <kanakkumarmahala000@gmail.com>
2024-10-03 18:36:48 +0530

    test

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,26 +3,33 @@ import { Link } from "react-router-dom";
 import appwriteServices from "../appwrite/config";
 
 function PostCard({ $id, featuredImage, title, content }) {
+  const wpm = 500;
+  const contentWords = content.length;
+  const readTime = Math.ceil(contentWords / wpm);
+
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100  py-4 flex flex-row justify-between border-b-2 border-gray-400 sm:h-60">
+      <div className="w-full bg-gray-100  py-4 flex flex-row justify-between border-b-2 border-gray-400 sm:h-60 gap-2">
         <div className="flex flex-col justify-between gap-2 w-3/4">
-          <div>
-            <h2 className=" text-xl sm:text-4xl font-bold">{title}</h2>
-            <p
-              className=" text-sm sm:text-xl line-clamp-3"
-              dangerouslySetInnerHTML={{ __html: content }}
-            ></p>
+          <div className="flex flex-col justify-between gap-3 sm:gap-12">
+            <div className="flex flex-col gap-3">
+              <h2 className=" text-xl sm:text-3xl font-fractul font-semibold text-gray-800">{title}</h2>
+              <p
+                className=" text-sm sm:text-lg line-clamp-3 font-fractul text-gray-700 "
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></p>
+            </div>
+            <div>
+              <p className="text-sm sm:text-base">{readTime} mins read</p>
+            </div>
           </div>
-
-         
         </div>
 
         <div className="justify-center mb-4 w-1/4">
           <img
             src={appwriteServices.getFilePreview(featuredImage)}
             alt={title}
-            className=" rounded-xl h-full w-full "
+            className=" rounded-md h-full w-full "
           />
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Container } from "../components";
 import PostCard from "../components/PostCard";
 import { BackgroundBeamsWithCollision } from "../components/ui/background-beams-with-collision";
 import HeroContainer from "../components/container/HeroContainer";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -24,18 +25,20 @@ function Home() {
               <h2 className="text-4xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black  font-sans tracking-tight pb-2">
                 Welcome to{" "}
                 <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-                  <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                  <div className=" font-fractul absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink [text-shadow:0_0_rgba(0,0,0,0.1)]">
                     <span className=""> blogAble</span>
                   </div>
-                  <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-400 via-violet-700 to-violet-500 py-4">
+                  <div className="font-fractul relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-400 via-violet-700 to-violet-500 py-4">
                     <span className=""> blogAble</span>
                   </div>
                 </div>
               </h2>
               <p className="mx-auto max-w-[700px] text-xl md:text-xl">
-                Exploring ideas, sharing insights, and discussing the latest
+                Exploring ideas and discussing the latest
                 trends in technology and beyond.
               </p>
+              <Link to={"/all-posts"}><button className="bg-gradient-to-r from-purple-400 via-violet-700 to-violet-500 text-white px-4 py-1 rounded-md mt-4 font-fractul">Explore</button></Link>
+              
             </div>
           </BackgroundBeamsWithCollision>
         </HeroContainer>
